8626c092502276dd2a2d2baeb3145e700adfb6e6
MD SAIF <mdsaif0405@gmail.com>
2021-12-31 23:44:30 +0530

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,12 +1,15 @@
 import os
 
+#List of options
 print("1. shutdown ")
 print("2. hibernate")
 print("3. restart")
 print("4. lock")
 print("5. exit")
 
+#input choice
 choice = int(input("Input option : "))
+#choice selected from option menu
 if(choice>=1 and choice<=4):
     if choice==1:
         os.system("shutdown /s /t 2")
